df13291824e4702f738109f98dc8463bb5143ce6
shabarish <shabarishkera@gmail.com>
2023-10-15 19:40:41 +0530

    added no internet page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from 'react'
+import NoInternet from './NoInternet'
 function App() {
+  useEffect(()=>{
+
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello</p>
+      <NoInternet>
+        {/* Your internet-dependent components go here */}
+        {/* For example, a chat component, a news feed, etc. */}
+      </NoInternet>
+      
     </div>
   );
 }
---
+++ src/NoInternet.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function NoInternet({ children }) {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const updateOnlineStatus = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    window.addEventListener('online', updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+
+    return () => {
+      window.removeEventListener('online', updateOnlineStatus);
+      window.removeEventListener('offline', updateOnlineStatus);
+    };
+  }, []);
+
+  return isOnline ? children : <p>You are currently offline. Please check your internet connection.</p>;
+}
+
+export default NoInternet;
