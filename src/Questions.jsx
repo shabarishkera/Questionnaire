bc87e0e6e90258b77fa0282ed435d3477a73e60a
shabarish <shabarishkera@gmail.com>
2023-10-16 18:05:42 +0530

    done rendering questions'

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
 import { useLocation } from 'react-router-dom'
 export default function Questions() {
-    const p=useLocation();
-    console.log(p.state.res)
+  const [qns,setqns]=useState([]);
+  const p=useLocation();
+  useEffect(()=>
+  {
+  
+    setqns(p.state.res);
+
+
+  },[])
+    
+   console.log(qns)
   return (
     <div>
-        this is quesits page
+      {qns.map((item, index) => {
+            return(
+          <div className="col-md" key={index}>
+           {item.question}
+          </div>
+            )
+})}
       
     </div>
   )
